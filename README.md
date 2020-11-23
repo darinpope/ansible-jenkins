2007c823b01807c14dc02ef8d8575c8d9932d9b5
Darin Pope <darin@planetpope.com>
2020-11-23 10:24:16 -0500

    refactor to create user and credential

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ansible-jenkins
 
 ansible 2.9.15
+
+Inspiration from:
+
+* https://www.linkedin.com/pulse/automating-jenkins-binary-installation-mohamed-mostafa/
---
+++ centos/7/02-jcasc-part-1/playbooks/files/init.groovy.d/init_20_create_vagrant_userpass_credential.groovy
@@ -0,0 +1,26 @@
+import jenkins.model.Jenkins
+import com.cloudbees.plugins.credentials.domains.Domain
+import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl
+import com.cloudbees.plugins.credentials.CredentialsScope
+
+File disableScript = new File(Jenkins.get().getRootDir(), ".disable-create-vagrant-userpass-credential")
+if (disableScript.exists()) {
+    logger.info("DISABLE create vagrant userpass credential")
+    return
+}
+
+instance = Jenkins.get()
+domain = Domain.global()
+store = instance.getExtensionList("com.cloudbees.plugins.credentials.SystemCredentialsProvider")[0].getStore()
+
+usernameAndPassword = new UsernamePasswordCredentialsImpl(
+  CredentialsScope.GLOBAL,
+  "vagrant",
+  "vagrant",
+  "vagrant",
+  "vagrant",
+)
+
+store.addCredentials(domain, usernameAndPassword)
+instance.save()
+disableScript.createNewFile()
